5e6a82ebca51861696bf75d836ba4727ac378b1b
K. M. Rajib Faysal <kmrajibfaysal@gmail.com>
2022-05-12 06:51:35 +0600

    added mongoDB connection and made simple api to provide services

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
+/* eslint-disable no-multi-spaces */
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
+const { MongoClient, ServerApiVersion } = require('mongodb');
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -12,6 +14,35 @@ app.use(express.json());
 app.get('/', (req, res) => {
     res.send(`Doctors portal server running on port ${port}`);
 });
+
+// Database connection
+
+// eslint-disable-next-line prettier/prettier
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.fakac.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    serverApi: ServerApiVersion.v1,
+});
+
+const run = async () => {
+    try {
+        await client.connect();
+        console.log('Database connected');
+        const serviceCollection = client.db('doctors-portal').collection('services');
+        app.get('/service', async (req, res) => {
+            const query = {};
+            const cursor = serviceCollection.find(query);
+            const services = await cursor.toArray();
+            res.send(services);
+        });
+    } finally {
+        //
+    }
+};
+
+run().catch(console.dir);
+
 // Listener
 app.listen(port, () => {
     console.log(`Doctors portal server running on port ${port}`);
